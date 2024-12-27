5e071589095a28c2610ec875329ba6ef6602b31d
Sagarthakur18 <sagarthakur8456@gmail.com>
2024-12-27 11:53:28 +0530

    packages

---
+++ cloudinaryConn.js
@@ -0,0 +1,11 @@
+const cloudinary = require('cloudinary').v2;
+
+// configuration
+cloudinary.config({ 
+    cloud_name: 'drtgq0any', 
+    api_key: '427833225974426', 
+    api_secret: 'yHl26sTuJtNycg7pxhzVoypRDKk' ,
+    // timeout: 60000, 
+});
+
+module.exports = cloudinary;
